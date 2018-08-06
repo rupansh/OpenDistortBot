f14b604f67c81e3fb855bc01cc2044dd36c6062f
Rupansh Kek <rupanshsekar@hotmail.com>
2018-08-07 00:29:56 +0530

    distortion_bot.py: random name of downloaded image
    Increase the power of distortion by a bit as well
    change /help output :V

--- distortion_bot.py
+++ distortion_bot.py
@@ -1,5 +1,6 @@
 import telebot
 import os
+import random
 
 TOKEN = "ENTER_YOUR_TOKEN"
 bot = telebot.TeleBot(TOKEN)
@@ -7,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 user = bot.get_me()
 
 def distort(fname):
-    boi = "convert "+fname+" -liquid-rescale 320x320 -implode 0.25 result/"+fname
+    boi = "convert "+fname+" -liquid-rescale 320x320 -implode 0.30 result/"+fname
     os.system(boi)
 
 
@@ -20,20 +21,21 @@ The source is available at - https://github.com/rupansh/OpenDistortBot""")
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, "Send me an image to distort it!")
+    bot.reply_to(message, "Send me an image to distort it! Your photos aren't stored! To prevent server from hitting its limit, the photo is downscaled to 320x320")
 
 
 @bot.message_handler(content_types=['photo'])
 def photudl(message):
-    file_info = bot.get_file(message.photo[2].file_id)
+    file_info = bot.get_file(message.photo[-1].file_id)
     download = bot.download_file(file_info.file_path)
-    with open('base.jpg', 'wb') as new_file:
+    file_name = str(random.randint(1,101))+".jpg"
+    with open(file_name, 'wb') as new_file:
         new_file.write(download)
-    distort("base.jpg")
-    photo = open('result/base.jpg', 'rb')
+    distort(file_name)
+    photo = open('result/'+file_name, 'rb')
     bot.send_photo(message.chat.id, photo)
-    os.remove("base.jpg")
-    os.remove("result/base.jpg")
+    os.remove(file_name)
+    os.remove("result/"+file_name)
 
 
 bot.polling()
