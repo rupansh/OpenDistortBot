c6f3da3d78ea0a12cfb98fd6da9ecff8c2e2632b
Rupansh Kek <rupanshsekar@hotmail.com>
2018-08-06 18:58:03 +0530

    distortion_bot.py: Add an option for help command
    Also change paths and command for linux as Windows sucks

--- distortion_bot.py
+++ distortion_bot.py
@@ -7,15 +7,20 @@ bot = telebot.TeleBot(TOKEN)
 user = bot.get_me()
 
 def distort(fname):
-    boi = "magick "+fname+" -liquid-rescale 320x320 -implode 0.25 result\\"+fname
+    boi = "convert "+fname+" -liquid-rescale 320x320 -implode 0.25 result/"+fname
     os.system(boi)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """Hey! This is a bot based on the distortion bot(@DistortBot). Since its not open source, I decided to make an open 
-    source version. Currently it only supports distorting image and it doesn't have power control (yet). 
-    The source is available at - https://github.com/rupansh/OpenDistortBot""")
+    bot.reply_to(message, """Hey! This is a bot based on the distortion bot(@DistortBot). Since its not open source, I decided to make an open
+source version. Currently it only supports distorting image and it doesn't have power control (yet).
+The source is available at - https://github.com/rupansh/OpenDistortBot""")
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Send me an image to distort it!")
 
 
 @bot.message_handler(content_types=['photo'])
@@ -25,9 +30,10 @@ def photudl(message):
     with open('base.jpg', 'wb') as new_file:
         new_file.write(download)
     distort("base.jpg")
-    photo = open('result\\base.jpg', 'rb')
+    photo = open('result/base.jpg', 'rb')
     bot.send_photo(message.chat.id, photo)
     os.remove("base.jpg")
-    os.remove("result\\base.jpg")
+    os.remove("result/base.jpg")
+
 
 bot.polling()
